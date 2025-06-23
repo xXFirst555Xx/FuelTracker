2b64001d8707b9ae35a6a4c2ad462d4355c162d9
xXFirst555Xx <firsttan4@gmail.com>
2025-06-23 14:32:47 +0700

    test: use importlib.metadata

--- tests/test_entrypoints_launch.py
+++ tests/test_entrypoints_launch.py
@@ -1,11 +1,11 @@
 import subprocess
 import sys
-import pkg_resources
+from importlib.metadata import entry_points
 
 
 def test_entrypoints_launch():
     assert "fueltracker" in {
-        ep.name for ep in pkg_resources.iter_entry_points("console_scripts")
+        ep.name for ep in entry_points(group="console_scripts")
     }
     code = subprocess.check_output([sys.executable, "-m", "fueltracker", "--check"])
     assert b"MainWindow OK" in code
