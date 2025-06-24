b43e0d5dbd2b0c0d0f074f672d97f0dd1ff8128e
xXFirst555Xx <firsttan4@gmail.com>
2025-06-24 13:11:33 +0700

    Localize auto fill checkbox

--- src/views/dialogs/ui_add_entry_dialog.py
+++ src/views/dialogs/ui_add_entry_dialog.py
@@ -216,7 +216,11 @@ class Ui_AddEntryDialog(object):
         )
         # endif // QT_CONFIG(tooltip)
         self.autoFillCheckBox.setText(
-            QCoreApplication.translate("AddEntryDialog", "Auto-fill fuel usage", None)
+            QCoreApplication.translate(
+                "AddEntryDialog",
+                "\u0e01\u0e23\u0e2d\u0e01\u0e1b\u0e23\u0e34\u0e21\u0e32\u0e13\u0e40\u0e0a\u0e37\u0e49\u0e2d\u0e40\u0e1e\u0e25\u0e34\u0e07\u0e2d\u0e31\u0e15\u0e42\u0e19\u0e21\u0e31\u0e15\u0e34",
+                None,
+            )
         )
 
     # retranslateUi
